b36b022a5bc360a093c158a23da5070124c1791a
Jana Wengenroth <jana.wengenroth@haw-hamburg.de>
2016-04-26 16:05:42 +0200

    changed client

--- client/src/main/client/client.java
+++ client/src/main/client/client.java
@@ -1,23 +1,35 @@
 package main.client;
 
+import sun.misc.IOUtils;
+
+import javax.xml.ws.Response;
+import java.io.IOException;
+
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by Jana Mareike on 26.04.2016.
  */
 public class client {
-    // Create an URL and a HTTP connection
-    URL url = new URL( "http://vs.haw/people" );
-    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-    connection.setRequestMethod( "GET" );
-    connection.setDoOutput(true);
 
-    if(body != null) {// Body?
-        connection.getOutputStream().write(body.getBytes());
+    public static Response createClient() throws IOException {
+        // Create an URL and a HTTP connection
+        URL url = new URL("http://localhost:4567/player");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setDoOutput(true);
+
+        if (body != null) {// Body?
+            connection.getOutputStream().write(body.getBytes());
+        }
+        connection.connect();// Do it!
+        // Get the response
+        int code = connection.getResponseCode();
+        // Which stream is available depends on the return code...
+        String body = code < 400 ? IOUtils.toString(connection.getInputStream())
+                : IOUtils.toString(connection.getErrorStream());
+        return new Response(code, body)throws IOException;
     }
-    connection.connect();// Do it!
-    // Get the response
-      int code = connection.getResponseCode();
-    // Which stream is available depends on the return code...
-    String body = code < 400 ? IOUtils.toString(connection.getInputStream())
-                     : IOUtils.toString(connection.getErrorStream());
-    return new Response(code, body);
 }
